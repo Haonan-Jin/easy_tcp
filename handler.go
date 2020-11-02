ac75c8e120f443e08b83b1265098c4fe130787d8
Haonan_Jin <1229984199@qq.com>
2020-11-02 10:34:27 +0800

    add notes

--- handler.go
+++ handler.go
@@ -2,7 +2,12 @@ package goland
 
 // A handler to handle decoded message
 type Handler interface {
+	// Implementation will be called when get a request or response.
+	// Msg's type depend on func Decoder's return type.
 	HandleMsg(ctx ConnectionHandler, msg interface{})
+
+	// Implementation will be called when an error occurred.
+	// Handle errors in the implementation.
 	HandleErr(ctx ConnectionHandler, err error)
 }
 
--- message_coder.go
+++ message_coder.go
@@ -1,10 +1,10 @@
 package goland
 
-// encode your data in this func
-// you need only encode the message part
-// this frame work will add the header
-// witch describes your message bytes len
+// Encode your msg to bytes
+// You need only encode the message part,
+// this framework will add the header automatically
+// witch describes your message bytes len.
 type Encoder func(message interface{}) []byte
 
-// decode unpacked bytes to a interface{}
+// Decode unpacked bytes to any type you want.
 type Decoder func(unpacked []byte) (interface{}, error)
