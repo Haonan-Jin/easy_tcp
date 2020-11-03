acf333ee76d25e2621ea86737ddcfac7100edf50
Haonan_Jin <1229984199@qq.com>
2020-11-03 16:21:49 +0800

    fixed an error

--- client.go
+++ client.go
@@ -71,6 +71,7 @@ func (tc *TcpClient) Dial() {
 			i, e := tc.conn.Read(buffer)
 			if e != nil {
 				if errors.Is(e, io.EOF) {
+					tc.cleanBuffer()
 					tc.buffer.Write(buffer)
 					tc.parseReadBytes()
 					continue
@@ -85,6 +86,10 @@ func (tc *TcpClient) Dial() {
 			tc.parseReadBytes()
 		}
 	}()
+}
+
+func (tc *TcpClient) cleanBuffer() {
+	tc.buffer = bytes.NewBuffer(nil)
 }
 
 func (tc *TcpClient) parseReadBytes() {
